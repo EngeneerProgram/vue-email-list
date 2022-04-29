8621e1a042f97417eb397ec86c157b3e13cf77c8
Rosario Garufi <saro.garufi1@gmail.com>
2022-04-29 15:22:05 +0200

    esercizio completato

--- assets/app.js
+++ assets/app.js
@@ -2,13 +2,19 @@ const app = new Vue(
     {
         el: "#app",
         data: {
-            data: "",
+            emails: [],
         },
         mounted(){
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(response =>{
+            for(let i =0; i<10; i++){
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(response =>{
                 console.log(this);
-                this.data = response.data;
+                this.emails.push(response.data.response)
+                console.log(this.emails);
             })
+
+            }
+            
+            
         }
 
         
